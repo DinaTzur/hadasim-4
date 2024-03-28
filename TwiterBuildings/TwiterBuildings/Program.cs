a87caf09c896c5f8e9f68b985e69a8f3e0a63644
DinaTzur <79421086+DinaTzur@users.noreply.github.com>
2024-03-28 12:49:02 +0200

    twiterBuildings

--- TwiterBuildings/TwiterBuildings/Program.cs
+++ TwiterBuildings/TwiterBuildings/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿//Prints that repeat themselves in several places or can be used in the future for more things
 const string getActionErrorMessage = "Wrong input !!!\n";
 const string getActionMessage = "Select Action\n" +
     "1. Rectangle\n" +
@@ -8,7 +8,7 @@ const string getTriangleActionMessage = "Select Action\n" +
     "1. Calculate perimeter\n" +
     "2. Print triangle";
 
-
+//Reference to further calculation according to the user's choice of shape
 int action;
 do
 {
@@ -24,7 +24,7 @@ do
             break;
     }
 } while (action != 3);
-
+//Getting shape dimensions parameters
 int GetMetric(string metric)
 {
     Console.WriteLine($"Input {metric}:");
@@ -34,7 +34,7 @@ int GetMetric(string metric)
     return res;
 }
 
-
+//Receiving variables, choosing a desired continuation and printing the extent or sending it to the triangle printing function.
 void HandleTriangle()
 {
     int height = GetMetric("height");
@@ -51,7 +51,7 @@ void HandleTriangle()
     {
         if(width%2 == 0 || height*2<width) 
         {
-            Console.WriteLine("Can't print triangle");
+            Console.WriteLine("Can't print triangle, Invalid dimensions for printing.");
         } 
         else
         {
@@ -59,43 +59,25 @@ void HandleTriangle()
         }
     }
 }
-
+//The triangle print
 void PrintTriangleToScreen(double width, double height)
 {
-    double groupNum = Math.Floor((width - 2) / 2);
-    double groupSize = Math.Floor((height - 2) / groupNum);
-    double firstGroupSize = height - (groupSize * groupNum + 2) + groupSize;
 
-    //option 1
+    double groupNum = Math.Max(Math.Floor((width - 2) / 2), 1);
+    double groupSize = Math.Floor((height - 2) / groupNum);
+    double firstGroupSize = Math.Max(height - (groupSize * groupNum + 2) + groupSize,1);
+
     for (int i = 1; i <= height; i++)
     {
         int group = GetGroupNum(i, firstGroupSize, groupSize);
-        int numOfStars = group * 2 - 1;
-        Console.WriteLine("".PadLeft(numOfStars, '*').PadLeft(Convert.ToInt32(Math.Floor(width - numOfStars) / 2) + numOfStars, ' '));
+        double numOfStars = Math.Min(group * 2 - 1, width); // Adjust for extreme case
+        Console.WriteLine("".PadLeft(Convert.ToInt32(numOfStars), '*').PadLeft(Convert.ToInt32(Math.Floor(width - numOfStars) / 2) + Convert.ToInt32(numOfStars), ' '));
     }
 
-    //option 2
-    double curGroupSize = firstGroupSize;
-    int numberOfStars = 3;
-    Console.WriteLine("".PadLeft(1, '*').PadLeft(Convert.ToInt32(Math.Floor(width - 1) / 2) + 1, ' '));
-
-    for (int i = 1; i <= groupNum; i++)
-    {
-
-        for (int j = 0; j < curGroupSize; j++)
-        {
-            Console.WriteLine("".PadLeft(numberOfStars, '*').PadLeft(Convert.ToInt32(Math.Floor(width - numberOfStars) / 2) + numberOfStars, ' '));
-        }
-        curGroupSize = groupSize;
-        numberOfStars += 2;
-    }
-
-    Console.WriteLine("".PadLeft(Convert.ToInt32(width), '*'));
+    
 }
 
-
-
-
+//An auxiliary function for calculating the number of asterisks needed in the row of the triangle
 int GetGroupNum(int i, double firstGroupSize, double groupSize)
 {
     if (i == 1)
@@ -106,6 +88,7 @@ int GetGroupNum(int i, double firstGroupSize, double groupSize)
         return Convert.ToInt32(Math.Ceiling((i - 1 - firstGroupSize) / groupSize) + 2);
 }
 
+//Accepting variables, and printing the perimeter or area of a rectangle according to an internal calculation
 void HandleRectangle()
 {
     int height = GetMetric("height");
@@ -120,7 +103,7 @@ void HandleRectangle()
         Console.WriteLine(height*2 + width*2);
     }
 }
-
+//Shape selection by the user
 int GetActionFromUser()
 {   
     do
@@ -142,7 +125,7 @@ int GetActionFromUser()
         }
     } while (true);
 }
-
+//Continued selection by the user on triangle data
 int GetTriangleActionFromUser()
 {
     do
